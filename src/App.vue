f1ec4970f4c3f38adf75284a36f10cc22cee1076
Your Name <you@example.com>
2023-05-24 16:33:29 +0200

    ADD header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script>
+import searchBar from "./components/searchBar.vue";
+import mainApp from "./components/mainApp.vue";
 
-<template></template>
+export default {
+  components: {
+    searchBar,
+    mainApp,
+  },
+};
+</script>
+
+<template>
+  <searchBar />
+  <mainApp />
+</template>
 
 <style scoped></style>
