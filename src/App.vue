56863ce5ed4dc412e48ae3530c2f81f804a43ece
Your Name <you@example.com>
2023-05-24 17:58:11 +0200

    milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,62 @@
 <script>
-import searchBar from "./components/searchBar.vue";
-import mainApp from "./components/mainApp.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppMovies from "./components/AppMovies.vue";
+import AppTvSeries from "./components/AppTvSeries.vue";
+import axios from "axios";
+import { store } from "./store";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
   components: {
-    searchBar,
-    mainApp,
+    AppHeader,
+    AppMain,
+    AppMovies,
+    AppTvSeries,
+  },
+
+  methods: {
+    RequestDataFromApi() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "21df2f399925b36ce74442455c67fc8a",
+            query: this.store.SearchBar,
+          },
+        })
+        .then((response) => (this.store.ArrMovies = response.data.results));
+
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: "21df2f399925b36ce74442455c67fc8a",
+            query: this.store.SearchBar,
+          },
+        })
+        .then((response) => (this.store.ArrTvSeries = response.data.results));
+    },
   },
 };
 </script>
 
 <template>
-  <searchBar />
-  <mainApp />
+  <AppHeader @performSearch="RequestDataFromApi" />
+  <AppMain />
 </template>
 
-<style scoped></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<script>
+import { store } from "../store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    emitSearchBar() {
+      this.$emit("performSearch");
+    },
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="flex_container">
+      <h1>BOOLFLIX</h1>
+      <div class="search">
+        <input
+          type="text"
+          placeholder="Cerca Film o SerieTV"
+          v-model="store.SearchBar"
+          @keyup.enter="emitSearchBar"
+        />
+        <button @click="emitSearchBar">CERCA</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.flex_container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 5rem;
+  padding-inline: 2rem;
+  background-color: black;
+
+  h1 {
+    color: red;
+  }
+
+  .search {
+    input {
+      margin-right: 1rem;
+    }
+  }
+}
+</style>
